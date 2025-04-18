0b7feaa90d4eb366d9e59d6d2775ac02d82facf3
me <@nothing.com>
2025-04-18 11:23:54 +0530

    adde loading animation

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const navLinks = document.getElementById("navLinks");
 menuIcon.addEventListener("click", () => {
     navLinks.classList.toggle("show");
 });
+// loading animation
+window.addEventListener("load", () => {
+    const loader = document.getElementById("load_container");
+    loader.classList.add("fade-out");
+  
+    // Optional: remove from DOM after fade-out
+    setTimeout(() => {
+      loader.remove();
+    }, 500); // match the CSS transition time
+  });
+  
+  
